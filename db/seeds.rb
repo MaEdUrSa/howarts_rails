2b71483ea994b078fa73804166ac07c4c92a59ee
meus <meus67@gmail.com>
2024-09-30 20:20:56 -0300

    Desafio Mod-5 dia-12 incorporando config Render

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-10.times do |i|
+20.times do |i|
     Character.create(name: Faker::Movies::HarryPotter.character, location: Faker::Movies::HarryPotter.location,
      house: Faker::Movies::HarryPotter.house)
 end
